88bafb0c57618c0e983fc3e545eefea209abc4fa
Pedrinscrk <o.pedro@estudante.ifmt.edu.br>
2025-04-11 23:08:36 -0400

    Oi

--- public/js/home.js
+++ public/js/home.js
@@ -7,3 +7,21 @@ window.addEventListener('scroll', function() {
         section.classList.add('animate-on-scroll');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animado');
+                observer.unobserve(entry.target); // Para de observar após a animação ocorrer uma vez
+            }
+        });
+    }, {
+        threshold: 0.5 // Define quando o elemento é considerado visível (50% neste caso)
+    });
+
+    const elementosAnimados = document.querySelectorAll('.processo-etapa-insano');
+    elementosAnimados.forEach(elemento => {
+        observer.observe(elemento);
+    });
+});
